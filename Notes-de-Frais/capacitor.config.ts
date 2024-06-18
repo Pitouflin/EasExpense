4fdf3a3a62b4f814b43b8d2ede4b208286b35a1c
Pitouflin <jules.legruel@viacesi.fr>
2024-06-18 17:16:04 +0200

    Implement MySQL DB

---
+++ Notes-de-Frais/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Notes-de-Frais',
+  webDir: 'dist'
+};
+
+export default config;
